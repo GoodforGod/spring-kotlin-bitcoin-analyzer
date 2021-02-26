85e8691c4d62281ac5b6ac8f658b672b22e2bffb
Anton Kurako <goodforgod.dev@gmail.com>
2021-02-26 19:53:54 +0300

    [0.9.0-SNAPSHOT] BitcoinBlockController.kt refactored BtcClientTests.kt and BitcoinBlockController.kt tests added

--- src/main/kotlin/com/arrival/crypto/bitcoinanalyzer/controller/BitcoinBlockController.kt
+++ src/main/kotlin/com/arrival/crypto/bitcoinanalyzer/controller/BitcoinBlockController.kt
@@ -1,10 +1,14 @@
 package com.arrival.crypto.bitcoinanalyzer.controller
 
 import com.arrival.crypto.bitcoinanalyzer.service.BlockHashCalculator
+import com.arrival.crypto.bitcoinanalyzer.service.BlockchainService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.server.ResponseStatusException
 import reactor.core.publisher.Mono
 
@@ -12,11 +16,14 @@ import reactor.core.publisher.Mono
  * @author Anton Kurako (GoodforGod)
  * @since 24.2.2021
  */
-@Controller("/bitcoin/block")
-class BitcoinBlockController(private val hashCalculator: BlockHashCalculator) {
+@RestController("/bitcoin/block")
+class BitcoinBlockController(@Qualifier("bitcoin") private val service: BlockchainService) {
 
-    @GetMapping("/subhash")
-    fun getSubHash(from: Long, to: Long): Mono<String> {
+    /**
+     * We can describe contracts via swagger and provide swagger-ui for better service use\exploitation\documentation
+     */
+    @GetMapping("/subhash", produces = [MediaType.TEXT_PLAIN_VALUE])
+    fun getSubHash(from: Int, to: Int): Mono<String> {
         if(from < 0)
             return Mono.error(ResponseStatusException(HttpStatus.BAD_REQUEST, "'from' block height can't be 0 or less"))
         if(to < from)
@@ -24,6 +31,8 @@ class BitcoinBlockController(private val hashCalculator: BlockHashCalculator) {
         if((to - from) > 100)
             return Mono.error(ResponseStatusException(HttpStatus.BAD_REQUEST, "Range can not be more than 100 blocks per request"))
 
-        return hashCalculator.getLongestSubHashByRange(from, to)
+        return service.getLongestSubHashByHeightRange(from.toLong(), to.toLong())
+            .filter { it.isNotEmpty() }
+            .switchIfEmpty(Mono.error(ResponseStatusException(HttpStatus.NOT_FOUND, "Sub hash not exist for such range")))
     }
 }
---
+++ src/test/kotlin/com/arrival/crypto/bitcoinanalyzer/controller/BitcoinBlockControllerTests.kt
@@ -0,0 +1,50 @@
+package com.arrival.crypto.bitcoinanalyzer.controller
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.http.HttpStatus
+
+/**
+ * @author Anton Kurako (GoodforGod)
+ * @since 26.2.2021
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WebMvcTest(BitcoinBlockController::class)
+class BitcoinBlockControllerTests(@Autowired val restTemplate: TestRestTemplate) : Assertions() {
+
+    @Test
+    fun `valid sub hash is calculated`() {
+        val entity = restTemplate.getForEntity<String>("/bitcoin/block/subhash?from=646940&to=646941")
+        assertEquals(HttpStatus.OK, entity.statusCode)
+        assertEquals("2989", entity.body)
+    }
+
+    @Test
+    fun `from less than 0 is rejected`() {
+        val entity = restTemplate.getForEntity<String>("/bitcoin/block/subhash?from=-1&to=5")
+        assertEquals(HttpStatus.BAD_REQUEST, entity.statusCode)
+    }
+
+    @Test
+    fun `to less than from is rejected`() {
+        val entity = restTemplate.getForEntity<String>("/bitcoin/block/subhash?from=5&to=3")
+        assertEquals(HttpStatus.BAD_REQUEST, entity.statusCode)
+    }
+
+    @Test
+    fun `to more than integer is rejected`() {
+        val entity = restTemplate.getForEntity<String>("/bitcoin/block/subhash?from=64694000000&to=6469410000000")
+        assertEquals(HttpStatus.BAD_REQUEST, entity.statusCode)
+    }
+
+    @Test
+    fun `range more than 100 is rejected`() {
+        val entity = restTemplate.getForEntity<String>("/bitcoin/block/subhash?from=100&to=300")
+        assertEquals(HttpStatus.BAD_REQUEST, entity.statusCode)
+    }
+}
